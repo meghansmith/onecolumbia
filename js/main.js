988b7f56d4fdf2fef4e91ca49070b97fad81589e
Meghan Smith <meghansmithsc@gmail.com>
2015-02-16 14:15:32 -0500

    Add masonry to landing page

--- js/main.js
+++ js/main.js
@@ -3,5 +3,11 @@ $(document).ready(function(){
 	$(".menuHide").addClass('hide');
 	$('#menuButton').click(function(){
 		$(".menuHide").slideToggle('fast');
-	})
+	});
+
+	var container = document.querySelector('#container');
+	var msnry = new Masonry( container, {
+	itemSelector: '.masonry'
 });
+});
+
